d5b3b4bbf4dba053152a81308e3fd845606c7856
jenishsorathiya <sorathiya.jenish93@gmail.com>
2026-03-17 17:05:17 +1100

    half footer done.

---
+++ includes/footer.php
@@ -0,0 +1,12 @@
+<footer class="footer">
+    <div class="container">
+    <div class="row">
+        <div class="col-md-4 footer-brand">
+            <h4>DineMate</h4>
+            <p>modern reservation system platform for old canberra Inn. book tables instantly, avoid waiting times, and enjoy a seamless dining experience.</p>
+            <div class="social-icons">
+                <a href="#"><i class="fab fa-facebook"></i></a>
+                <a href="#"><i class="fab fa-twitter"></i></a>
+
+                <a href="#"><i class="fab fa-instagram"></i></a>
+                
