9413d17e27b24ba804028026905a43d1463ff7fe
jenishsorathiya <sorathiya.jenish93@gmail.com>
2026-03-17 17:20:09 +1100

    other part of footer done.

--- includes/footer.php
+++ includes/footer.php
@@ -9,4 +9,22 @@
                 <a href="#"><i class="fab fa-twitter"></i></a>
 
                 <a href="#"><i class="fab fa-instagram"></i></a>
-                
+                <a href="#"><i class="fab fa-linkedin"></i></a>
+            </div>
+            <button onclick="scrollTopPage()" class="back-top">↑ Back to Top</button>
+        </div>
+        <div class="col-md-4">
+            <h5>Site Map</h5>
+            <ui>
+                <li><a href="index.php">Homepage</a></li>
+                <li><a href="#">Book Table</a></li>
+                <li><a href="#">Login</a></li>
+                <li><a href="#">register</a></li>
+                <li><a href="contact.php">Contact Us</a></li>
+            </ui>
+        </div>
+
+        
+
+
+    
