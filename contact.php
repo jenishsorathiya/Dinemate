903563493336d405383dda29819ccbfdd6e6e426
jenishsorathiya <sorathiya.jenish93@gmail.com>
2026-03-23 19:38:24 +1100

    some changes - contact page

--- contact.php
+++ contact.php
@@ -54,16 +54,16 @@
         <h1>Contact Us</h1>
         <p>We Would lovw to hear from your</p>
     </div>
-</selection>
+</section>
 
-<section class="selection">
+<section class="section">
     <div class="container">
         <div class="row g-4">
             <div class="col-md-5">
                 <div class="contact-info">
                     <<h4 class="mb-4">Restaurant Information</h4>
                     <p><i class="fa fa-location-dot contact-icon"></i> Old Canberra Inn, 195 Mouat St, Lyneham ACT 2602, Australia</p>
-                    <p><i class="fa fa-phone contact-icon"></i> +61 2 6248 7424</p>
+                    <p><i class="fa fa-phone contact-icon"></i> +61 421108735</p>
                     <p><i class="fa fa-envelope contact-icon"></i> info@oldcanberrainn.com</p>
                     <p><i class="fa fa-clock contact-icon"></i> Mon – Sun : 11:00 AM – 11:00 PM</p>
                 </div>
