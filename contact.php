c14b1d61b7e291995b295ce417a8ddb7ad86b012
jenishsorathiya <sorathiya.jenish93@gmail.com>
2026-03-23 18:20:42 +1100

    contact body part complete

--- contact.php
+++ contact.php
@@ -22,4 +22,47 @@
                     <p><i class="fa fa-clock contact-icon"></i> Mon – Sun : 11:00 AM – 11:00 PM</p>
                 </div>
             </div>
-            
+             <div class="col-md-7">
+                <div class="contact-card">
+                    <h4 class="mb-4">Reservation Help</h4>
+                    <form>
+                        <div class="row">
+                            <div class="col-md-6 mb-3">
+                                <label>Your Name</label>
+                                <input type="text" class="form-control" required>
+                            </div>
+                            <div class="col-md-6 mb-3">
+                                <label>Email</label>
+                                <input type="email" class="form-control" required>
+                            </div>
+                            <div class="col-md-6 mb-3">
+                                <label>Phone</label>
+                                <input type="text" class="form-control">
+                            </div>
+                            <div class="col-md-6 mb-3">
+                                <label>Subject</label>
+                                <input type="text" class="form-control">
+                            </div>
+                            <div class="col-12 mb-3">
+                                <label>Message</label>
+                                <textarea class="form-control" rows="4"></textarea>
+                            </div>
+                        </div>
+                        <button class="btn btn-contact"><i class="fa fa-paper-plane"></i> Send Message</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="section bg-light">
+    <div class="container">
+        <h3 class="text-center mb-5">Find Us Here</h3>
+        <div class="map">
+            <iframe src="https://www.google.com/maps?q=Old%20Canberra%20Inn&output=embed" width="100%" height="400" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+        </div>
+    </div>
+</section>
+
+<?php include "includes/footer.php"; ?>
