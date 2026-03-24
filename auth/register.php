66a1109b500a15d011ee3e3b4f15cea670b2b07b
Jhalak001 <cihe240503@student.cihe.edu.au>
2026-03-24 19:00:26 +1100

    added title in register form./jhalak

--- auth/register.php
+++ auth/register.php
@@ -1,4 +1,12 @@
+<?php session_start(); ?>
+
+<!DOCTYPE html>
 <html>
+<head>
+<title>DineMate | Register</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap" rel="stylesheet">
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" rel="stylesheet">
 
-
-</html>
+<style>    
