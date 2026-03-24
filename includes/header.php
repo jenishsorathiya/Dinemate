7ce9b401b6fdcfdf6187e279b3b3e33dfbc5b423
jenishsorathiya <sorathiya.jenish93@gmail.com>
2026-03-24 13:39:50 +1100

    roll back wrong brach

--- includes/header.php
+++ includes/header.php
@@ -54,19 +54,7 @@ if (session_status() === PHP_SESSION_NONE) {
             background: #e0a800;
             transform: scale(1.05);
         }
-        .btn-logout {
-        border: 1px solid white;
-        color: white;
-        padding: 8px 18px;
-        border-radius: 25px;
-        transition: 0.3s;
-        text-decoration: none;
-        }
 
-        .btn-logout:hover {
-        background: white;
-        color: black;
-        }
     </style>
 </head>
 <body>
@@ -83,24 +71,6 @@ if (session_status() === PHP_SESSION_NONE) {
                 <a href="about.php">About</a>
                 <a href="contact.php">Contact</a>
 
-                <?php if(isset($_SESSION['user_id'])): ?>
-                    <!-- Logged In User Links -->
-                    <a href="bookings/my-bookings.php">My Bookings</a>
-                    <a href="bookings/book-table.php" class="btn btn-book ms-3">
-                        <i class="fa fa-calendar-check"></i> Book Table
-                    </a>
-                    <a href="auth/logout.php" class="btn btn-logout ms-3">
-                        Logout
-                    </a>
-                <?php else: ?>
-                    <!-- Guest Links -->
-                    <a href="auth/login.php">Login</a>
-                    <a href="auth/register.php" class="btn btn-book ms-3">
-                        Register
-                    </a>
-                <?php endif; ?>
-
             </div>
         </div>
     </div>
-</nav>
