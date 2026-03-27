c15f99b7cdea030d1313795a60733f027c3761b7
prajapatisujal53 <prajapatisujal53@gmail.com>
2026-03-28 10:48:21 +1100

    added SQL query to booking and user id

--- bookings/cancel-booking.php
+++ bookings/cancel-booking.php
@@ -1,3 +1,10 @@
 <?php
 require_once "../config/db.php";
 require_once "../includes/session-check.php";
+
+$id = intval($_GET['id']);
+
+$stmt = $pdo->prepare("
+UPDATE bookings SET status='cancelled'
+WHERE booking_id=? AND user_id=?
+");
