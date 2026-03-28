93d76e89e0b121062f857dd4ba8b77bf2f577db8
prajapatisujal53 <prajapatisujal53@gmail.com>
2026-03-28 11:05:56 +1100

    added connection to logged in user

--- bookings/cancel-booking.php
+++ bookings/cancel-booking.php
@@ -8,3 +8,9 @@ $stmt = $pdo->prepare("
 UPDATE bookings SET status='cancelled'
 WHERE booking_id=? AND user_id=?
 ");
+
+$stmt->execute([$id, $_SESSION['user_id']]);
+
+header("Location: my-bookings.php");
+exit();
+?>
