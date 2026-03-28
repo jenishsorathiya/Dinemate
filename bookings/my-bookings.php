4441298cc51bef02a5f20ac5b4543c18fa02e553
prajapatisujal53 <prajapatisujal53@gmail.com>
2026-03-29 10:40:38 +1100

    created user bookings table using join query

--- bookings/my-bookings.php
+++ bookings/my-bookings.php
@@ -1,1 +1,24 @@
- 
+<?php
+require_once "../config/db.php";
+require_once "../includes/session-check.php";
+require_once "../includes/functions.php";
+
+if(!isCustomer()){
+header("Location: ../auth/login.php");
+exit();
+}
+
+$stmt = $pdo->prepare("
+SELECT b.*, t.table_number
+FROM bookings b
+JOIN restaurant_tables t
+ON b.table_id = t.table_id
+WHERE b.user_id = ?
+ORDER BY b.booking_date DESC
+");
+
+$stmt->execute([$_SESSION['user_id']]);
+$bookings = $stmt->fetchAll(PDO::FETCH_ASSOC);
+?>
+
+<?php include "../includes/header.php"; ?>
