2d48ae1a6bfdf42b97044220e267deb79963e036
prajapatisujal53 <prajapatisujal53@gmail.com>
2026-03-29 22:38:13 +1100

    add validation to enusre all booking fields

--- bookings/process-booking.php
+++ bookings/process-booking.php
@@ -7,4 +7,16 @@ if($_SERVER["REQUEST_METHOD"] !== "POST"){
     redirect("book-table.php");
 }
 
+$user_id = $_SESSION['user_id'];
+
+$date = sanitize($_POST['booking_date']);
+$time = sanitize($_POST['booking_time']);
+$guests = intval($_POST['number_of_guests']);
+$table_id = intval($_POST['table_id']);
+$special = sanitize($_POST['special_request']);
+
+if(empty($date) || empty($time) || empty($guests) || empty($table_id)){
+    die("All fields are required.");
+}
+
 
