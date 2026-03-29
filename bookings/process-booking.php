ceb9dfa22e1afd7f2d7d70cb54530527102da9e0
prajapatisujal53 <prajapatisujal53@gmail.com>
2026-03-29 14:30:44 +1100

    added process booking

---
+++ bookings/process-booking.php
@@ -0,0 +1,8 @@
+<?php
+require_once "../config/db.php";
+require_once "../includes/session-check.php";
+require_once "../includes/functions.php";
+
+if($_SERVER["REQUEST_METHOD"] !== "POST"){
+    redirect("book-table.php");
+}
