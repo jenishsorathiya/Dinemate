1633c79ee78cd7ff0e23c6c163083cddc0803115
werakz <werakdongol04@gmail.com>
2026-04-03 20:25:46 +1100

    navigation issue solved

--- includes/header.php
+++ includes/header.php
@@ -2,6 +2,42 @@
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
+
+$documentRootPath = realpath($_SERVER['DOCUMENT_ROOT'] ?? '') ?: '';
+$projectRootPath = realpath(__DIR__ . '/..') ?: '';
+$basePath = '';
+
+if ($documentRootPath !== '' && $projectRootPath !== '') {
+    $normalizedDocumentRoot = str_replace('\\', '/', $documentRootPath);
+    $normalizedProjectRoot = str_replace('\\', '/', $projectRootPath);
+    if (strpos($normalizedProjectRoot, $normalizedDocumentRoot) === 0) {
+        $basePath = str_replace('\\', '/', substr($normalizedProjectRoot, strlen($normalizedDocumentRoot)));
+    }
+}
+
+$basePath = rtrim($basePath, '/');
+$requestPath = str_replace('\\', '/', (string) (parse_url($_SERVER['REQUEST_URI'] ?? '', PHP_URL_PATH) ?: ''));
+$relativeRequestPath = ltrim($requestPath, '/');
+
+if ($basePath !== '' && strpos($requestPath, $basePath . '/') === 0) {
+    $relativeRequestPath = ltrim(substr($requestPath, strlen($basePath)), '/');
+}
+
+$navUrl = static function (string $path) use ($basePath): string {
+    return ($basePath !== '' ? $basePath : '') . '/' . ltrim($path, '/');
+};
+
+$isActivePath = static function (array $paths) use ($relativeRequestPath): bool {
+    foreach ($paths as $path) {
+        if ($relativeRequestPath === ltrim($path, '/')) {
+            return true;
+        }
+    }
+
+    return false;
+};
+
+$isLoggedInUser = isset($_SESSION['user_id']);
 ?>
 <!DOCTYPE html>
 <html>
@@ -46,6 +82,9 @@ if (session_status() === PHP_SESSION_NONE) {
         .nav-links a:hover {
             color: #f4b400;
         }
+        .nav-links a.is-active {
+            color: #f4b400;
+        }
         .btn-book {
             background: #f4b400;
             color: black;
@@ -84,32 +123,32 @@ if (session_status() === PHP_SESSION_NONE) {
 
 <nav class="navbar navbar-modern navbar-expand-lg">
     <div class="container-fluid">
-        <a class="logo" href="index.php">DineMate</a>
+        <a class="logo" href="<?php echo htmlspecialchars($navUrl('index.php'), ENT_QUOTES, 'UTF-8'); ?>">DineMate</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navMenu">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse justify-content-end" id="navMenu">
             <div class="nav-links">
-                <a href="index.php">Home</a>
-                <a href="about.php">About</a>
-                <a href="menu.php">Menu</a>
-                <a href="contact.php">Contact</a>
-                <?php if(isset($_SESSION['user_id'])): ?>
+                <a href="<?php echo htmlspecialchars($navUrl('index.php'), ENT_QUOTES, 'UTF-8'); ?>" class="<?php echo $isActivePath(['', 'index.php']) ? 'is-active' : ''; ?>">Home</a>
+                <a href="<?php echo htmlspecialchars($navUrl('about.php'), ENT_QUOTES, 'UTF-8'); ?>" class="<?php echo $isActivePath(['about.php']) ? 'is-active' : ''; ?>">About</a>
+                <a href="<?php echo htmlspecialchars($navUrl('menu.php'), ENT_QUOTES, 'UTF-8'); ?>" class="<?php echo $isActivePath(['menu.php']) ? 'is-active' : ''; ?>">Menu</a>
+                <a href="<?php echo htmlspecialchars($navUrl('contact.php'), ENT_QUOTES, 'UTF-8'); ?>" class="<?php echo $isActivePath(['contact.php']) ? 'is-active' : ''; ?>">Contact</a>
+                <?php if($isLoggedInUser): ?>
                     <!-- Logged In User Links -->
-                    <a href="bookings/my-bookings.php">My Bookings</a>
-                    <a href="bookings/book-table.php" class="btn btn-book">
+                    <a href="<?php echo htmlspecialchars($navUrl('bookings/my-bookings.php'), ENT_QUOTES, 'UTF-8'); ?>" class="<?php echo $isActivePath(['bookings/my-bookings.php', 'bookings/modify-booking.php']) ? 'is-active' : ''; ?>">My Bookings</a>
+                    <a href="<?php echo htmlspecialchars($navUrl('bookings/book-table.php'), ENT_QUOTES, 'UTF-8'); ?>" class="btn btn-book <?php echo $isActivePath(['bookings/book-table.php', 'bookings/booking-confirmation.php']) ? 'is-active' : ''; ?>">
                         <i class="fa fa-calendar-check"></i> Book Table
                     </a>
-                    <a href="auth/logout.php" class="btn btn-logout">
+                    <a href="<?php echo htmlspecialchars($navUrl('auth/logout.php'), ENT_QUOTES, 'UTF-8'); ?>" class="btn btn-logout">
                         Logout
                     </a>
                 <?php else: ?>
                     <!-- Guest Links -->
-                    <a href="bookings/book-table.php" class="btn btn-book">
+                    <a href="<?php echo htmlspecialchars($navUrl('bookings/book-table.php'), ENT_QUOTES, 'UTF-8'); ?>" class="btn btn-book <?php echo $isActivePath(['bookings/book-table.php', 'bookings/booking-confirmation.php']) ? 'is-active' : ''; ?>">
                         <i class="fa fa-calendar-check"></i> Book Table
                     </a>
-                    <a href="auth/login.php">Login</a>
-                    <a href="auth/register.php" class="btn btn-book">
+                    <a href="<?php echo htmlspecialchars($navUrl('auth/login.php'), ENT_QUOTES, 'UTF-8'); ?>" class="<?php echo $isActivePath(['auth/login.php']) ? 'is-active' : ''; ?>">Login</a>
+                    <a href="<?php echo htmlspecialchars($navUrl('auth/register.php'), ENT_QUOTES, 'UTF-8'); ?>" class="btn btn-book <?php echo $isActivePath(['auth/register.php']) ? 'is-active' : ''; ?>">
                         Register
                     </a>
                 <?php endif; ?>
